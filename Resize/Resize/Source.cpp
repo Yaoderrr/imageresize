31a8fb3cbb801fd45954cc3414926cb1317a94a9
philipchang8787 <chingyao301@gmail.com>
2019-09-02 13:25:06 +0800

    upload 201909021325

--- Resize/Resize/Source.cpp
+++ Resize/Resize/Source.cpp
@@ -8,3 +8,15 @@ distruption:讀寫bmp file
 #include <stdio.h>
 #include <inttypes.h>
 typedef unsigned char uch;
+
+/*bmp檔案結構*/
+
+#pragma pack(2)
+struct BmpFileHeader
+{
+	uint16_t bfTybe;/*說明bmp的類型*/
+	uint32_t bfSize;/*實際上的檔案大小*/
+	uint16_t bfReserved1;/*需保留，值設定為0*/
+	uint16_t bfReserved2;/**/
+	uint32_t bfOffBits;/*說明文件開始到圖像數據之間的偏移量，由此可知起始位址*/
+};
