89cdb905de5920ec0f8e62224ee1b354aba2469b
philipchang8787 <chingyao301@gmail.com>
2019-09-02 11:59:22 +0800

    add based file

---
+++ Resize/Resize/Source.cpp
@@ -0,0 +1,10 @@
+/*°Ñ¦Ò: https://gist.github.com/hunandy14/143d6036a846d1d53c9cff2be807cf1d */
+/*
+coder:Chang Ching Yao
+date:20190902
+distruption:Åª¼gbmp file
+*/
+#include<stdlib.h>
+#include <stdio.h>
+#include <inttypes.h>
+typedef unsigned char uch;
